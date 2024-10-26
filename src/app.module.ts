5cc4e66ef86840a36f1b41a7f4469e907c94262f
Saeed Norouzi <saeednorouzi98@gmail.com>
2024-10-26 12:16:36 +0330

    Feat(AppModule): Initialize 'Postgres' database connection
    Description:
    Add typeorm configuration into the AppModule file, and initialize
    Postgres databse connection.
    
    Logs:
    [Modified][AppModule]: Add typeorm copnfiguration.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,28 @@
 import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
-	imports: [],
+	imports: [
+		//? In NestJS, TypeORM is a popular choice for managing database interactions using the Active Record or
+		//? Data Mapper patterns. TypeORM simplifies handling complex SQL operations by using object-oriented
+		//? programming principles, allowing you to define entities as classes and perform database operations
+		//? through methods rather than writing raw SQL.
+
+		//? To use TypeORM in NestJS, start by configuring it in `AppModule`. Import `TypeOrmModule`
+		//? and use the `forRoot` method to specify connection details such as database type, host, port,
+		//? username, password, and entities.
+		TypeOrmModule.forRoot({
+			type: "postgres",
+			host: "localhost",
+			port: 5432,
+			username: "postgres",
+			password: "root",
+			database: "typeorm",
+			autoLoadEntities: true,
+			entities: [],
+			synchronize: true,
+		}),
+	],
 	controllers: [],
 	providers: [],
 })
